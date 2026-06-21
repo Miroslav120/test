e6b875217f5a7980502eb67161e166cbc9c4130e
Miroslav <miroslav@example.com>
2026-06-21 19:12:06 +0300

    Add cat icon and design to app

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.helloworld;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello, World!");
-        setContentView(tv);
+        setContentView(R.layout.activity_main);
     }
 }
